d81804d2b9f29c1350bb60517c0b48184b89b402
HongRae-Kim <ghdfo7882@gmail.com>
2025-09-17 23:21:57 +0900

    [BE9]김홍래

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.5.5"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com"
+version = "0.0.1-SNAPSHOT"
+description = "sbb-exam"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    runtimeOnly ("com.h2database:h2")
+    implementation ("org.springframework.boot:spring-boot-starter-data-jpa")
+    testImplementation ("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly ("org.junit.platform:junit-platform-launcher")
+    implementation ("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation ("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect")
+    implementation ("org.springframework.boot:spring-boot-starter-validation")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
